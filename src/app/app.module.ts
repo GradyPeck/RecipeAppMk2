9b716efc7e84c90679fffd31751ada67cfe25ed4
Ryan DeVree <ryan.devree@gmail.com>
2020-02-10 00:42:09 -0500

    without CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './main/main.component';
-import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
 
 
 @NgModule({
@@ -19,7 +18,7 @@ import { SearchFormComponent } from './search-form/search-form.component';
     RecipeListComponent,
     FavoritesPageComponent,
     MainComponent,
-    SearchFormComponent
+    SearchCriteriaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipes } from '../recipes';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-search-criteria',
   templateUrl: './search-criteria.component.html',
   styleUrls: ['./search-criteria.component.css']
 })
-export class SearchCriteriaComponent implements OnInit {
 
-  constructor() { }
+export class SearchCriteriaComponent implements OnInit {
+  @Input()
+  post : Recipes;
+
+  @Input()
+  recipes : Recipes[] = [];
+
+  open : boolean = false;
+
+  constructor(public service: RecipesService) { }
 
   ngOnInit() {
   }
 
+  openForm() {
+    if (this.open === false) {
+      this.open = true;
+    }
+    else {
+      this.open = false;
+    }
+  }
+  
+  closeSearchForm() {
+    this.open = false;
+  }
 }
